35aa5937028481d48da6f5df8fdccf5387caf463
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-11-19 00:27:29 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // (Optional) Export as a standalone site
-  // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
-  output: "standalone", // Feel free to modify/remove this option
+  // (Optional) Export as a static site
+  // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+  output: "export", // Feel free to modify/remove this option
 
-  // Indicate that these packages should not be bundled by webpack
-  experimental: {
-    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+  // Override the default webpack configuration
+  webpack: (config) => {
+    // Ignore node-specific modules when bundling for the browser
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
   },
 };
 
